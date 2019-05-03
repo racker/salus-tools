faf2cc6411ec6f51f58522f5cfc92d8076107e04
Adam Martinek <harageth@users.noreply.github.com>
2019-05-03 07:44:52 -0700

    gave javadocs and udated the parameters for SwaggerJsonConverter (#4)
    * gave javadocs and udated the parameters for SwaggerJsonConverter
    
    * added an argDelimiter
    
    * made sure to take into account that String.split doesnt give an empty string if there is nothing on the right hand side of the delimiter
    
    * used splitValues since I already had it and didn't need to split again
    
    * oops got a little overzealous with ripping out a piece of code
    
    * forgot about checking to make sure tenant existed in the path
    
    * forgot to use i on the String.split
    
    * removed logging statements

--- src/main/java/com/rackspace/salus/salus_tools/converter/SwaggerJsonConverter.java
+++ src/main/java/com/rackspace/salus/salus_tools/converter/SwaggerJsonConverter.java
@@ -13,39 +13,53 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class SwaggerJsonConverter {
+    static final String argDelimiter="=";
 
+    /**
+     * This function
+     * @param args
+     * 0. path to the location that the swagger.json is and to where we will output the resultant json from this
+     * 1. should be "tenant/{tenantId}"= or {tenandId}= depending on whether the API has /tenant in the path
+     * 2. Each argument should be in the format "StringToBeReplaced"="replacementText".
+     *    If you want to remove a string then it should be "StringToBeReplaced"=    with no replacmentText
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         String content = new Scanner(new File(args[0]+"/swagger.json")).useDelimiter("\\Z").next();
         ObjectNode root = (ObjectNode)mapper.readTree(content);
         Map<String, JsonNode> temp = new HashMap();
+
+        String newKey = null;
+        boolean containsTenant;
         for (Iterator<Map.Entry<String, JsonNode>> it = root.get("paths").fields(); it.hasNext(); ) {
             Map.Entry<String, JsonNode> elt = it.next();
-            if (elt.getKey().contains("tenant"))
-            {
-                String newKey = elt.getKey().replace("tenant/{tenantId}/", "");
-                temp.put(newKey, elt.getValue());
-                /*
-                attempting to remove the parameters but is failing on array index out of bounds exception
-
-                elt.getValue().fields().forEachRemaining((webVerbs)->{
-                    int i = 0;
-                    for (Iterator<Map.Entry<String, JsonNode>> parameters = elt.getValue().get("parameters").fields(); parameters.hasNext(); ) {
-                        Map.Entry<String, JsonNode> parameter = parameters.next();
-                        if (parameter.getValue().get("name").asText().compareTo("tenantId") == 0) {
-                            //parameters is an array
-                            ((ArrayNode)temp.get(webVerbs.getKey()).get("parameters")).remove(i);
-                            break;
-                        }
-                        i++;
-                    }
-                });*/
-
-                it.remove();
-
-            } else {
-                it.remove();
+            newKey = elt.getKey();
+            containsTenant = newKey.contains("tenant");
+            for(int i = 1; i < args.length; i++) {
+                String[] splitValues = args[i].split(argDelimiter);
+                newKey = newKey.replace(splitValues[0], splitValues.length == 1? "" : splitValues[1]);
             }
+            /*
+            attempting to remove the parameters but is failing on array index out of bounds exception
+
+            elt.getValue().fields().forEachRemaining((webVerbs)->{
+                int i = 0;
+                for (Iterator<Map.Entry<String, JsonNode>> parameters = elt.getValue().get("parameters").fields(); parameters.hasNext(); ) {
+                    Map.Entry<String, JsonNode> parameter = parameters.next();
+                    if (parameter.getValue().get("name").asText().compareTo("tenantId") == 0) {
+                        //parameters is an array
+                        ((ArrayNode)temp.get(webVerbs.getKey()).get("parameters")).remove(i);
+                        break;
+                    }
+                    i++;
+                }
+            });*/
+            if(containsTenant) {
+                temp.put(newKey, elt.getValue());
+            }
+
+            it.remove();
         }
         ObjectNode pathNode = mapper.getNodeFactory().objectNode();
 
