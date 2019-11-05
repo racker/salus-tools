2e1459335b8cd891f27690c5699a76288a2ddb43
George Jahad <github@blackbirdsystems.net>
2019-11-05 01:20:16 -0800

    cleaned up env var support

---
+++ e2et/local_envs.sh
@@ -0,0 +1,7 @@
+export E2ET_MODE=local
+export E2ET_TENANT_ID=aaaaaa
+export E2ET_PUBLIC_API_URL=http://localhost:8080/
+export E2ET_ADMIN_API_URL=http://localhost:8888/
+export E2ET_CERT_DIR=../../dev/certs
+export E2ET_KAFKA_BROKERS=localhost:9092
+export E2ET_EVENT_TOPIC=salus.events.json
